e3c2a63c953aaa141d17bc0a3c04fd11ef067e46
Nathalie Paredes <nparedes614@gmail.com>
2020-10-28 19:13:53 -0500

    assignment commit

---
+++ 04-java-spring/02-spring-data-i/02-show/Show/src/main/java/com/Nathalie/show/services/BookService.java
@@ -0,0 +1,37 @@
+package com.Nathalie.show.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.Nathalie.show.models.Book;
+import com.Nathalie.show.repositories.BookRepository;
+
+@Service
+public class BookService {
+	private BookRepository bRepo;
+	public BookService(BookRepository repo) {
+		this.bRepo = repo;
+	}
+	//get all
+	public List<Book> getAllBooks(){
+		return this.bRepo.findAll();
+	}
+	//get one
+	public Book getOneBook(Long id) {
+		return this.bRepo.findById(id).orElse(null);
+	}
+	//create book
+	public Book createBook(Book newBook) {
+		return this.bRepo.save(newBook);
+	}
+	//update book
+	public Book updateBook(Book updatedBook) {
+		return this.bRepo.save(updatedBook);
+	}
+	//delete book
+	public String deleteBook(Long id) {
+		this.bRepo.deleteById(id);
+		return "Book" + id + "has been deleted";
+	}
+}
